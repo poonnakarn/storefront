1d6e56013f6a2a6ec3f19139a7b82c689214f389
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-01-11 22:56:18 +0700

    Overriding base queryset using get_queryset()

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from . import models
 
-# Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['title','unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['first_name', 'last_name', 'membership']
+    list_editable=['membership']
+    list_per_page = 10
+    ordering=['first_name', 'last_name']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=['title', 'products_count']
+    
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count('product'))
+
+# admin.site.register(models.Product)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer_name']
+    list_select_related = ['customer']
+    
+    def customer_name(self, order):
+        return order.customer
